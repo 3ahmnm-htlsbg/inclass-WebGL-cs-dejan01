9058b7401a8aaa1f9157c554da169b967f131de0
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2020-01-14 15:12:27 +0100

    create new C# Script, declare variables, try by using Debug.Log and Random.Range if numbers 0 and 1 are displayed in console

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    //public GameObject PosTopLeft;
+    //public GameObject PosTopRight;
+    //public GameObject PosBottomLeft;
+    //public GameObject PosBottomRight;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject ball;
+    
+    public void MoveTheBall(){
+        Debug.Log(Random.Range(0, 2));
+    }
+}
