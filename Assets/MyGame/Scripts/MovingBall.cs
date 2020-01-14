78fc40b91a89b9e938c570e54e5a669248fc8d59
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2020-01-14 14:01:00 +0100

    create Script and attach it to empty object

---
+++ Assets/MyGame/Scripts/MovingBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingBall : MonoBehaviour
+{
+
+    public GameObject Ball;
+    
+    [SerializeField]private GameObject[] BallPositions;
+
+
+    private int RandomNum()
+    {
+        return Random.Range(0, 4);
+    }
+ 
+
+    public void BallPos()
+    { 
+        Ball.transform.position = BallPositions[RandomNum()].transform.position;
+    }
+
+}
